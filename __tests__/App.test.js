64f13349649845202f0580cae8ea95091ec3cd61
Graham Langford <grahamlangford87@gmail.com>
2018-05-18 16:28:57 -0500

    fixed test suite

---
+++ __tests__/App.test.js
@@ -0,0 +1,10 @@
+// __tests__/App.js
+import React from "react";
+import App from "../App";
+
+import renderer from "react-test-renderer";
+
+it("renders without crashing", () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
